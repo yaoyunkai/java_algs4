159f0d7031aa53ddb15016cbc945056e1de60464
liberty <1052433260@qq.com>
2023-02-11 15:18:16 +0800

    add BinarySearch

---
+++ src/main/java/com/libyao/algs4/BinarySearch.java
@@ -0,0 +1,56 @@
+package com.libyao.algs4;
+
+import edu.princeton.stdlib.In;
+import edu.princeton.stdlib.StdIn;
+import edu.princeton.stdlib.StdOut;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    private BinarySearch() {
+    }
+
+    /**
+     * Returns the index of the specified key in the specified array.
+     *
+     * @param arr the array of integers, must be sorted in ascending order
+     * @param key the search key
+     * @return index of key in array {@code a} if present; {@code -1} otherwise
+     */
+    public static int indexOf(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < arr[mid]) hi = mid - 1;
+            else if (key > arr[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    /**
+     * Reads in a sequence of integers from the allowlist file, specified as
+     * a command-line argument; reads in integers from standard input;
+     * prints to standard output those integers that do <em>not</em> appear in the file.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // read the integers from a file
+        In in = new In("data/tinyAllowlist.txt");
+        int[] allowlist = in.readAllInts();
+
+        // sort the array
+        Arrays.sort(allowlist);
+
+        // read integer key from standard input; print if not in allowlist
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (BinarySearch.indexOf(allowlist, key) == -1) StdOut.println(key);
+        }
+    }
+
+}
