cfe8e377a4d21b359bac39e259a4a7e1cd3a501a
liberty yao <libyao@cisco.com>
2023-05-09 16:39:26 +0800

    add 3 class: Bag Queue Stack

---
+++ src/main/java/com/libyao/algs4/Bag.java
@@ -0,0 +1,108 @@
+/******************************************************************************
+ *  Compilation:  javac Bag.java
+ *  Execution:    java Bag < input.txt
+ *  Dependencies: StdIn.java StdOut.java
+ *
+ *  A generic bag or multiset, implemented using a singly linked list.
+ *
+ *  % more tobe.txt
+ *  to be or not to - be - - that - - - is
+ *
+ *  % java Bag < tobe.txt
+ *  size of bag = 14
+ *  is
+ *  -
+ *  -
+ *  -
+ *  that
+ *  -
+ *  -
+ *  be
+ *  -
+ *  to
+ *  not
+ *  or
+ *  be
+ *  to
+ *
+ ******************************************************************************/
+
+package com.libyao.algs4;
+
+import edu.princeton.stdlib.StdIn;
+import edu.princeton.stdlib.StdOut;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+public class Bag<E> implements Iterable<E> {
+    private Node<E> first;    // beginning of bag
+    private int n;               // number of elements in bag
+
+    // helper linked list class
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+
+    public Bag() {
+        first = null;
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void add(E e) {
+        // 头插法
+        Node<E> oldfirst = first;
+        first = new Node<>();
+        first.item = e;
+        first.next = oldfirst;
+        n++;
+    }
+
+    public Iterator<E> iterator() {
+        return new LinkedIterator(first);
+    }
+
+    private class LinkedIterator implements Iterator<E> {
+        private Node<E> current;
+
+        public LinkedIterator(Node<E> first) {
+            current = first;
+        }
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public E next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            E e = current.item;
+            current = current.next;
+            return e;
+        }
+    }
+
+    public static void main(String[] args) {
+        Bag<String> bag = new Bag<>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            bag.add(item);
+        }
+
+        StdOut.println("size of bag = " + bag.size());
+        for (String s : bag) {
+            StdOut.println(s);
+        }
+    }
+
+}
+
