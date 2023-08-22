06d5d4c4c8f5120b4d4015b18824545cf91a9630
liberty <1052433260@qq.com>
2023-08-22 23:11:08 +0800

    DoubleLinkedList

---
+++ src/main/java/com/libyao/algs4/chp2/DoubleLinkedList.java
@@ -0,0 +1,11 @@
+package com.libyao.algs4.chp2;
+
+public class DoubleLinkedList {
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
+    }
+
+}
---
+++ src/main/java/com/libyao/algs4/chp2/Person.java
@@ -0,0 +1,56 @@
+package com.libyao.algs4.chp2;
+
+public class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        checkAge(age);
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private void checkAge(int age) {
+        if (age > 149 || age < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void setAge(int age) {
+        checkAge(age);
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (age == o.age) return 0;
+        return age > o.age ? 1 : -1;
+    }
+
+    public static void main(String[] args) {
+        Person tom = new Person("tom", 18);
+        Person tom1 = new Person("tom", 19);
+        int i = tom.compareTo(tom1);
+        System.out.println(i);
+    }
+}
