fc5d9a290a7c393e0f9b645bc3951aa93b4f1169
liberty yao <libyao@cisco.com>
2023-08-29 14:21:41 +0800

    LSD

--- src/main/java/com/libyao/algs4/chp5/LSD.java
+++ src/main/java/com/libyao/algs4/chp5/LSD.java
@@ -2,5 +2,37 @@ package com.libyao.algs4.chp5;
 
 public class LSD {
 
+    private LSD() {
+    }
+
+    public static void sort(String[] a, int w) {
+        int n = a.length;
+        int R = 256;
+
+        String[] aux = new String[n];
+
+        for (int d = w - 1; d >= 0; d--) {
+            int[] count = new int[R + 1];
+
+
+            for (String s : a) {
+                count[s.charAt(d) + 1]++;
+            }
+
+            for (int r = 0; r < R; r++) {
+                count[r + 1] += count[r];
+            }
+
+            for (String s : a) {
+                aux[count[s.charAt(d)]++] = s;
+            }
+
+            System.arraycopy(aux, 0, a, 0, n);
+        }
+
+    }
+
+    public static void main(String[] args) {
+    }
 
 }
