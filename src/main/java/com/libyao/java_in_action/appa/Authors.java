7122d1b5e62fb64c730ef08da5f93edbffa3454b
yunk <1052433260@qq.com>
2026-01-24 22:35:52 +0800

    remove somethings

--- src/main/java/com/libyao/java_in_action/appa/Authors.java
+++
@@ -1,9 +0,0 @@
-package com.libyao.java_in_action.appa;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Authors {
-    Author[] value();
-}
