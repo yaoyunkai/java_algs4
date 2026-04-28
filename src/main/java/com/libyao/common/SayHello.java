8427852dc7ced85d43c820d2f763adad00fe7123
yunk <1052433260@qq.com>
2026-04-28 15:05:36 +0800

    update

--- src/main/java/com/libyao/common/SayHello.java
+++ src/main/java/com/libyao/common/SayHello.java
@@ -1,7 +1,9 @@
 package com.libyao.common;
 
+
 public class SayHello {
     public static void main(String[] args) {
+
         System.out.println("hello world");
         System.out.println("你好世界");
     }
